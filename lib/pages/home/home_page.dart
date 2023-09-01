9085a3b0397d674c9d97940a97126ae877172d9a
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2023-09-01 16:35:42 +0700

    feat: add score counter

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:rock_paper_scissors/utilities/constants.dart';
 
@@ -9,9 +11,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final int won = 0;
-  final int draw = 0;
-  final int lost = 0;
+  late String userChoice = '';
+  late String botChoice = '';
+  int won = 0;
+  int draw = 0;
+  int lost = 0;
+  final List<String> choices = ['👊🏻', '🖐🏻', '✌🏻'];
+  static const String rock = '👊🏻';
+  static const String paper = '🖐🏻';
+  static const String scissors = '✌🏻';
+  final Map<String, String> winConditions = {
+    rock: scissors,
+    paper: rock,
+    scissors: paper,
+  };
+
+  String determineWinner(String userChoice, String botChoice) {
+    if (userChoice == botChoice) {
+      return 'draw';
+    } else if (winConditions[userChoice] == botChoice) {
+      return 'won';
+    } else {
+      return 'lost';
+    }
+  }
+
+  void _scoreCounter(String newUserChoice) {
+    final newBotChoice = choices[Random().nextInt(choices.length)];
+    final winner = determineWinner(userChoice, newBotChoice);
+
+    setState(() {
+      userChoice = newUserChoice;
+      botChoice = newBotChoice;
+
+      if (winner == 'draw') {
+        draw++;
+      } else if (winner == 'won') {
+        won++;
+      } else {
+        lost++;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +70,7 @@ class _HomePageState extends State<HomePage> {
     return Container(
       padding: EdgeInsets.all(defaultMargin),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           _scoreText(title: 'Won', score: won),
           _scoreText(title: 'Draw', score: draw),
@@ -44,10 +85,17 @@ class _HomePageState extends State<HomePage> {
       children: [
         Text(
           '$title ',
+          style: const TextStyle(
+            fontSize: 18,
+          ),
           textScaleFactor: 1.0,
         ),
         Text(
           score.toString(),
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: bold,
+          ),
           textScaleFactor: 1.0,
         ),
       ],
@@ -89,19 +137,38 @@ class _HomePageState extends State<HomePage> {
   Widget _gameButton() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        _gameButtonIcon(icon: '👊🏻', tooltip: 'Rock'),
-        _gameButtonIcon(icon: '🖐🏻', tooltip: 'Paper'),
-        _gameButtonIcon(icon: '✌🏻', tooltip: 'Scissors'),
-      ],
+      children: choices.map((icon) {
+        final tooltip = getTitleForIcon(icon);
+        return _gameButtonIcon(icon: icon, tooltip: tooltip);
+      }).toList(),
     );
+  }
+
+  String getTitleForIcon(String icon) {
+    switch (icon) {
+      case rock:
+        return 'Rock';
+      case paper:
+        return 'Paper';
+      case scissors:
+        return 'Scissors';
+      default:
+        return '';
+    }
   }
 
   Widget _gameButtonIcon({required String icon, required String tooltip}) {
     return Tooltip(
       message: tooltip,
-      child: TextButton(
-        onPressed: () {},
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: whiteColor,
+          padding: EdgeInsets.all(defaultMargin),
+          shape: const CircleBorder(),
+        ),
+        onPressed: () {
+          _scoreCounter(icon);
+        },
         child: Text(
           icon,
           style: const TextStyle(
